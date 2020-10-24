bb8d4fa2565bdf812f83e6b12502e8001da0c26d
eeveelise <evelise.dib@loft.com.br>
2020-10-24 14:36:58 -0300

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AUHAUAHUAHUAHAUHAUAHUAHUAHAUHAUHAUAHUA';
   teste = 34 + 2;
+  disabledButton = false;
+  avatar = "https://avatars2.githubusercontent.com/u/22223345?s=460&u=464e1e65dfb54dcebfd508e7697e123963e776b7&v=4";
+  private _customClass = "";
+  private _ngClassCustom = false;
 
-  soma(x, y) {
-    return x + y;
+  get ngClassCustom() {
+    return this._ngClassCustom;
   }
 
+  // get customClass(){
+  //   return this._customClass;
+  // }
 
+  enviar() {
+    this._ngClassCustom = !this._ngClassCustom;
+    // if(this._customClass === "") {
+    //   this._customClass = "hide";
+    // } else {
+    //   this._customClass = "";
+    // }
+  }
+
+  input(){
+    console.log('ta digitando')
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './header/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule
